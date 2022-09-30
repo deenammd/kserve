8156f15802fc942c83e38bdbd1c6048e82dedf6d
Deena Mukundan <92586802+deenammd@users.noreply.github.com>
2022-09-30 21:10:53 +0530

    Update test.py

--- test.py
+++ test.py
@@ -16,30 +16,9 @@ isvc = V1beta1InferenceService(api_version=api_version,
                                spec=V1beta1InferenceServiceSpec(
                                predictor=V1beta1PredictorSpec(
                                sklearn=(V1beta1SKLearnSpec(
-                                   storage_uri="gs://kfserving-samples/models/sklearn/iris"))))
+                                   storage_uri="gs://kfserving-examples/models/sklearn/1.0/model"))))
 )
 KServe = KServeClient()
 KServe.create(isvc)
-from kserve import utils
-from kserve import V1beta1InferenceService
-from kserve import V1beta1InferenceServiceSpec
-from kserve import V1beta1PredictorSpec
-from kserve import V1beta1SKLearnSpec
-
-namespace = utils.get_default_target_namespace()
-name='sklearn-iris'
-kserve_version='v1beta1'
-api_version = constants.KSERVE_GROUP + '/' + kserve_version
-
-isvc = V1beta1InferenceService(api_version=api_version,
-                               kind=constants.KSERVE_KIND,
-                               metadata=client.V1ObjectMeta(
-                                   name=name, namespace=namespace, annotations={'sidecar.istio.io/inject':'false'}),
-                               spec=V1beta1InferenceServiceSpec(
-                               predictor=V1beta1PredictorSpec(
-                               sklearn=(V1beta1SKLearnSpec(
-                                   storage_uri="gs://kfserving-samples/models/sklearn/iris"))))
-)
-KServe = KServeClient()
 isvc_resp = KServe.get(name, namespace=namespace)
 print(isvc_resp)
